595fa0a28fe326bbc30b6a71abb4a34f146abec0
Jassocc <clnearle@gmail.com>
2023-06-16 04:45:00 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int max;
+
+for (max = 0; max < 10; max++)
+{
+putchar(max + '0');
+if (x < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n')
+
+return (0);
+}
