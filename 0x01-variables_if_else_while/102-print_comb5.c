df6fd2c11c96c6bf205026e354259ed1e075aace
Jassocc <clnearle@gmail.com>
2023-06-16 06:41:08 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 int alpha;
 int beta;
 
-for (alpha = 0; alpha < 98; alpha++)
+for (alpha = 0; alpha < 100; alpha++)
 {
-for (beta = alpha + 1; beta < 98; beta++)
+for (beta = alpha + 1; beta < 100; beta++)
 {
 putchar((alpha / 10) + '0');
 putchar((alpha % 10) + '0');
@@ -20,7 +20,7 @@ putchar(' ');
 putchar((beta / 10) + '0');
 putchar((beta % 10) + '0');
 
-if (alpha != 99 || beta != 99)
+if (alpha != 100 || beta != 100)
 {
 putchar(',');
 putchar(' ');
