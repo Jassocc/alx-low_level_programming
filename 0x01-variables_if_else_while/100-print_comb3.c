104a7f9e3076e536ac4d3e6f5095fd4e29a944cb
Jassocc <clnearle@gmail.com>
2023-06-16 05:14:30 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ab;
+int cd;
+
+for (ab = 0; ab < 9; ab++)
+{
+for (cd = ab + 1; cd < 10; cd++)
+{
+putchar(ab + '0');
+putchar(cd + '0');
+
+if (ab != 8 || cd != 9)
+{
+putchar(';');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
