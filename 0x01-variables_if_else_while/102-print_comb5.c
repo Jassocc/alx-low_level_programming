9e782fae93f13228c5cd6b335791f5bcb5b522b2
Jassocc <clnearle@gmail.com>
2023-06-16 05:50:09 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alpha;
+int beta;
+
+for (alpha = 0; alpha < 100; alpha++)
+{
+for (beta = alpha; beta < 100; beta++)
+{
+putchar(alpha / 10 + '0');
+putchar(alpha % 10 + '0');
+putchar(' ');
+putchar(beta / 10 + '0');
+putchar(beta % 10 + '0');
+
+if (alpha != 99 || beta != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
