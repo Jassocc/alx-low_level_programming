e915fe90cb4a0c4e4038e37e6055184705d8bba2
Jassocc <clnearle@gmail.com>
2023-06-20 10:14:47 -0700

    task 14

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the sum of the even-valued terms, followed by a new line.
+ *Return: 0
+ */
+
+int main(void)
+{
+int previous;
+int current;
+int sum;
+
+while (current <= 4000000)
+{
+if (current % 2 == 0)
+{
+sum += current;
+}
+int next = previous + current;
+previous = current;
+current = next;
+}
+printf("%d\n", sum);
+
+return (0);
+}
