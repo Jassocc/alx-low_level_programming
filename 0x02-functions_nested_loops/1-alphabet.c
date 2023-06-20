8191d937695537029bcee79a4c6cfc97159bafb1
Jassocc <clnearle@gmail.com>
2023-06-20 03:07:52 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
+/**
+* print_alphabet - prints the entire alphabet
+* Return void
+*/
 void print_alphabet(void)
 {
 char alphabet;
-
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
-_putchar('\n')
+_putchar('\n');
 
 }
