b1c19fb753437d6448eddf2c1a7e31791b5683ee
Jassocc <clnearle@gmail.com>
2023-06-20 09:10:34 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of multiples
+ * Return: 0
+ */
+
+int main(void)
+{
+int num;
+int sum;
+
+sum = 0;
+
+for (num = 0, num < 1024; num++)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+{
+sum += num;
+}
+}
+printf("%d", sum);
+
+return (0);
+}
