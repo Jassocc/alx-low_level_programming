a605377e65e308c2ebc5d20d061c02a92ede68bd
Jassocc <clnearle@gmail.com>
2023-06-20 11:00:10 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98
+ * Return: 0
+ */
+
+int main(void)
+{
+int previous = 1;
+int current = 2;
+int count = 2;
+int next;
+
+printf("%d, %d, ", previous, current);
+
+while (count < 98)
+{
+next = previous + current;
+printf("%d, ", next);
+
+previous = current;
+current = next;
+count++;
+}
+printf("\n");
+
+return (0);
+}
