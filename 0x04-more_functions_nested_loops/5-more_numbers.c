cc92d967953dd526f20979167a11e63471c933a2
Jassocc <clnearle@gmail.com>
2023-06-22 04:41:09 -0700

    task 5 fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 int numerical;
 int lines;
 
-for (lines = 0; lines < 10; lines++)
+for (lines = 1; lines <= 10; lines++)
 {
 for (numerical = 0; numerical <= 14; numerical++)
 {
 if (numerical >= 10)
 {
-_putchar('1');
+_putchar((i / 10) + '0');
 _putchar((numerical % 10) + '0');
 }
 }
