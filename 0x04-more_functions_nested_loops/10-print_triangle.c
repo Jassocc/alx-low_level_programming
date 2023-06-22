108b175d516d798fdd0286792ba24bcf8c920886
Jassocc <clnearle@gmail.com>
2023-06-22 03:10:10 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangles using # in given size
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+int height, width, length;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (height = 1; height <= size; height++)
+{
+for (width = 1; width <= (size - height); height++)
+_putchar(' ');
+
+for (length = 1; length <= height; length++)
+_puthcar('#');
+
+_putchar('\n');
+}
+}
