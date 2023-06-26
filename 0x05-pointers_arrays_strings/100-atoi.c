7d0aa650bf02037a233009ab571cef9b6529d617
Jassocc <clnearle@gmail.com>
2023-06-26 09:16:23 -0700

    task 10 fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 /**
 * _atoi - convert to integer
 * @s: string to be converted
@@ -7,27 +6,36 @@
 */
 int _atoi(char *s)
 {
-int r = 0;
-int si = 1;
-int a = 0;
-if (s[a] == '-')
-{
-si = -1;
-a++;
-}
-else if (s[a] == '+')
-{
-a++;
-}
-while (s[a] >= '0' && s[a] <= '9')
-{
-if (r > (INT_MAX / 10) || (r == (INT_MAX / 10) && (s[a] - '0') > (INT_MAX % 10)))
-{
-return ((si == 1) ? INT_MAX : INT_MIN);
-}
-r = (r * 10) + (s[a] - '0');
-a++;
-}
+int r, a, c, len, f, digit;
+r = 0;
+a = 0;
+c = 0;
+len = 0;
+f = 0;
+digit = 0;
 
-return (r * si);
+while (s[len] != '\0')
+len++;
+
+while (r < len && f == 0)
+{
+if (s[r] == '-')
+++a;
+if (s[r] >= '0' && s[r] <= '9')
+{
+digit = s[r] - '0';
+if (a % 2)
+digit = -digit;
+c = c * 10 + digit;
+f = 1;
+if (s[r + 1] < '0' || s[r + 1] > '9')
+break;
+f = 0;
+}
+r++;
+}
+if (f == 0)
+return (0);
+
+return (n);
 }
