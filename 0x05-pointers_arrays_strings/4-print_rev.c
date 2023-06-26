04992913ccbdc420ab946acdeac829fc3b3c588c
Jassocc <clnearle@gmail.com>
2023-06-26 04:18:10 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in rerverse
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+int length = 0;
+int rev;
+
+for ((rev = length - 1); rev >= 0; rev--)
+{
+_putchar(*s);
+}
+_putchar('\n');
+}
