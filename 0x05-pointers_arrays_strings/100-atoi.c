2ca47565d5493e89f6759112521377e8c08b2979
Jassocc <clnearle@gmail.com>
2023-06-26 08:53:10 -0700

    task 10 fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <limits.h>
 /**
- * _atoi - convert to integer
- * @s: string to be converted
- * Return: integer
- */
+* _atoi - convert to integer
+* @s: string to be converted
+* Return: integer
+*/
 int _atoi(char *s)
 {
 int r = 0;
-int s = 1;
+int si = 1;
 int a = 0;
 if (s[a] == '-')
 {
-s = -1;
+si = -1;
 a++;
 }
 else if (s[a] == '+')
@@ -21,13 +21,13 @@ a++;
 }
 while (s[a] >= '0' && s[a] <= '9')
 {
-if (r > (INT_MAX / 10) || (r == (INT_MAX / 10) && (s[a] - '0') > (INT_MAX % 10)))
+if (r > (INT_MAX / 10) || (r == (INT_MAX / 10 && s[a] - '0') > (INT_MAX % 10)))
 {
-return ((s == 1) ? INT_MAX : INT_MIN);
+return ((si == 1) ? INT_MAX : INT_MIN);
 }
 r = (r * 10) + (s[a] - '0');
 a++;
 }
 
-return (r *s);
+return (r *si);
 }
