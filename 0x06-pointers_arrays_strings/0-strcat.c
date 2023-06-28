b18f12faf9282264fd47f255ca7f4935e27526ac
Jassocc <clnearle@gmail.com>
2023-06-28 01:46:43 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - add 2 strings together
+ * @dest: string 
+ * @src: string to be added
+ * Return: added string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int a = 0;
+int b = 0;
+
+while (dest[a] != '\0')
+{
+a++;
+}
+while (src[b] != '\0')
+{
+b++;
+a++;
+}
+dest[a] = '\0';
+
+return (dest);
+}
