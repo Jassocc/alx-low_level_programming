02f57f72065c6168aae7d8bea26851297eef143c
Jassocc <clnearle@gmail.com>
2023-06-29 04:03:08 -0700

    task 12 fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,35 +9,38 @@
 
 void print_buffer(char *b, int size)
 {
-int a = 0, c;
-if (size < 0)
+int a, c;
+if (size <= 0)
 {
 printf("\n");
 return;
 }
-while (a < size)
+for (a = 0; a < size; a += 10)
 {
-if (a % 10 == 0)
 printf("%08x: ", a);
-for (c = a; c < a + 9; c += 2)
+for (c = 0; c < 10; c++)
 {
-if ((c < size) && ((c + 1) < size))
-printf("%02x%02x ", b[c], b[c + 1]);
+if (a + c < size)
+printf("%02x", (unsigned char)b[a + c]);
 else
-{
-while (++c <= a + 10)
 printf("  ");
-printf("  ");
+if (c % 2 == 1)
+printf(" ");
 }
-}
-for (c = a; c < a + 9 && c < size; c++)
+for (c = 0; c < 10; c++)
 {
-if (b[c] >= 32 && b[c] <= 126)
-printf("%c", b[c]);
+if (a + c < size)
+{
+if (b[a + c] >= 32 && b[a + c] <= 126)
+printf("%c", b[a + c]);
 else
 printf(".");
 }
+else
+{
+printf(" ");
+}
+}
 printf("\n");
-a += 10;
 }
 }
