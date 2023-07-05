e8b3d328bdec9eb0328846cccbf616c2df30d214
Jassocc <clnearle@gmail.com>
2023-07-05 04:00:51 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * prime - divide by higher divisor
+ * @n: check if prime
+ * @div: divisor
+ * Return: 1 if prime, 0 if ot
+ */
+
+int prime(int n, int div)
+{
+	if (n % div == 0)
+	{
+		return (0);
+	}
+	if (n == div)
+	{
+		return (1);
+	}
+	return (prime(n, div + 1));
+}
+/**
+ * is_prime_number - check if prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	else if (n % 2 == 0 || n <= 1)
+	{
+		return (0);
+	}
+	return (prime(n, 2));
+}
