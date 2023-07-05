64b89589a7a1c482f618e1f5785fce802753bd3e
Jassocc <clnearle@gmail.com>
2023-07-05 04:13:52 -0700

    task 6 fixed logic in prime function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
 
 int prime(int n, int div)
 {
-	if (n % div == 0)
+	if (n % div == 0 || n < 2)
 	{
 		return (0);
 	}
-	if (n == div)
+	if (div * div > n)
 	{
 		return (1);
 	}
@@ -30,7 +30,7 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	else if (n % 2 == 0 || n <= 1)
+	if (n <= 1)
 	{
 		return (0);
 	}
