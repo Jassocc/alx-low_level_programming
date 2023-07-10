26a4af7c28fa8988e58038de6da24307dd0c02d8
Jassocc <clnearle@gmail.com>
2023-07-10 04:57:48 -0700

    task 5 update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,6 +3,55 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - check length
+ * @str: string
+ * Return: length
+ */
+
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+/**
+ * _strdup - duplicates string
+ * @str: string to duplicate
+ * Return: Null
+ */
+
+char *_strdup(char *str)
+{
+	int len, a;
+	char *duplication;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = _strlen(str);
+	duplication = malloc((len + 1) * sizeof(char));
+
+	if (duplication == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < len; a++)
+	{
+		duplication[a] = str[a];
+	}
+	duplication[len] = '\0';
+
+	return (duplication);
+}
+
+/**
  * word_counter - count amount of words
  * @str: string to count
  * Return: count
@@ -38,4 +87,4 @@ int word_counter(char *str)
 char **strtow(char *str)
 {
 	int word;
-	int ind = 0, 
+	int ind = 0,
