576edddc1bd061d1a8b13b388759f5a2d7d8c492
Jassocc <clnearle@gmail.com>
2023-07-13 10:28:00 -0700

    task 6 update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -71,4 +71,117 @@ char *iterate_zero(char *str)
  * get_pr - multiplies string of numbers
  * @prod: stor result
  * @multi: string
- * @digit: 
+ * @digit: single num
+ * @zero: leading zero
+ */
+
+void get_pr(char *prod, char *multi, int digit, int zero)
+{
+	int multi_len, num, find_len, ten;
+
+	multi_len = find_len(multi) - 1;
+	multi += multi_len;
+	while (*prod)
+	{
+		*prod = 'x';
+		prod++;
+	}
+	prod--;
+	while (zero--)
+	{
+		*prod = '0';
+		prod--;
+	}
+	for (; multi_len >= 0; multi_len--, multi--, prod--)
+	{
+		if (*multi < '0' || *multi > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		num = (*multi - '0') * digit;
+		*prod = (num % 10) + '0';
+		ten = num / 10;
+		*(prod - 1) += ten;
+	}
+}
+
+/**
+ * add_num - add  number from 2 strings
+ * @final_prod: storing final product
+ * @next_prod: next addition
+ * @next_length: length of added prod
+ */
+
+void add_num(char *final_prod, char *next_prod, int next_length)
+{
+	int ten, num1, num2;
+
+	while(*(final_prod + 1))
+	{
+		final_prod++;
+	}
+	while(*(next_prod + 1))
+	{
+		next_prod++;
+	}
+	ten = 0;
+	while (*final_prod != 'x')
+	{
+		num1 = (*final_prod - '0') + (*next_prod - '0') + ten;
+		*final_prod = (num1 % 10) +'0';
+		ten = num / 10;
+		final_prod--;
+		next_prod--;
+		next_length--;
+	}
+	while (next_length >= 0 && *next_prod !+ 'x')
+	{
+		num2 = (*next_prod - '0') + ten;
+		*final_prod = (num2 % 10) + '0';
+		ten = num2 / 10;
+		final_prod--;
+		next_prod--;
+		next_length--;
+	}
+	if (ten)
+	{
+		*final_prod = (ten % 10) + '0';
+	}
+}
+
+/**
+ * main - multiply 2 positives
+ * @argv: arguments passed
+ * @argc: argument counter
+ * Return: 0
+ */
+
+int main(int argc, int argv)
+{
+	char *num3, *num4, *final_prod, *next_prod;
+	int index, digit, printer;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num3 = argv[1];
+	num4 = argv[2];
+	if (num3 == '0')
+	{
+		num1 = iterate_zero(num3);
+	}
+	if (num4 == '0')
+	{
+		num4 = iterate_zero(num4);
+	}
+	if (*num3 == '\0' || *num4 == '\0')
+	{
+		printf("0\n");
+		retrun (0);
+	}
+	size = find_length(num3) + find_length(num4);
+	final_prod = create_xarray(size);
+	next_prod = create_xarray(size);
