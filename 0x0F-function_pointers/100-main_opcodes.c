02a2b7543332b3211b102bc34003d5b760c73957
Jassocc <clnearle@gmail.com>
2023-07-19 07:38:00 -0700

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print own opcodes
+ * @argc: counter
+ * @argv: arguments
+ * Return: 0 
+ */
+
+int main(int argc, char *argv[])
+{
+	int data, a = 0;
+	char *array
+
+		if (argc != 2)
+		{
+			printf("Error\n");
+			exit(1);
+		}
+	data = atoi(argv[1]);
+	if (data < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	array = (char *)main;
+
+	for (; a < data; a++)
+	{
+		if (a == data - 1)
+		{
+			printf("%02hhx\n", array[a]);
+			break;
+		}
+		printf("%02hhx", array[a]);
+	}
+	return (0);
+}
