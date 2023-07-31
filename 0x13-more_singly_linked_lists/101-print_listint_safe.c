5a9c4df7cbf2881320b240d89b0f0ded151a8138
Jassocc <clnearle@gmail.com>
2023-07-31 13:35:15 -0700

    task 1 update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,94 +1,34 @@
 #include "lists.h"
 
 /**
- * node_count - counts nodes
- * @head: pointer
- * Return: count
- */
-int node_count(const listint_t *head)
-{
-	int c;
-	const listint_t *fast, *slow;
-
-	slow = fast = head;
-	c = 0;
-	while (slow != NULL && fast != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		c++;
-		if (slow == fast)
-		{
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-				c++;
-			}
-			return (c);
-		}
-	}
-	return (0);
-}
-/**
- * list_loop - find loop in linked list
- * @head: list
- * Return: 0 if no loop
- */
-int list_loop(const listint_t *head)
-{
-	const listint_t *slow, *fast;
-
-	slow = fast = head;
-	while (slow != NULL && fast != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-/**
  * print_listint_safe - prints a listint_t linked list.
  * @head: list
  * Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int c, f;
-	size_t node;
-	const listint_t *temp;
+	size_t node = 0;
+	const listint_t *temp = head;
+	const listint_t *loop_node = NULL;
 
-	node = 0;
-	c = 0;
-	if (head == NULL)
+	while (temp)
 	{
-		exit(98);
-	}
-	f = list_loop(head);
-	if (f == 1)
-	{
-		c = node_count(head);
-		temp = head;
-		for (f = 0; f < c; f++)
+		printf("[%p] %d\n", (void *)temp, temp ->n);
+		node++;
+		if (temp == loop_node)
 		{
-			printf("[%p] %d\n", (void *)temp, temp->n);
-			node = node + 1;
-			temp = temp->next;
+			printf("-> [%p] %d\n", (void *)temp, temp->n);
+			break;
 		}
-	}
-	else if (f == 0)
-	{
-		temp = head;
-		while (temp != NULL)
+		temp = temp->next;
+		if (node % 2 == 0 && temp != NULL)
 		{
-			printf("[%p] %d\n", (void *)temp, temp->n);
-			node = node + 1;
-			temp = temp->next;
+			if (temp == loop_node)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				break;
+			}
+			loop_node = loop_node ? loop_node->next : head;
 		}
 	}
 	return (node);
