537cae10ff68b2c81bd4ebb894a5151397015d0d
Jassocc <clnearle@gmail.com>
2023-07-31 09:03:33 -0700

    betty refixture

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: list
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
