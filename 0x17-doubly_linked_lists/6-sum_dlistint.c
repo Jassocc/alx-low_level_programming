aef939f1f0119d2e9d8fd90f7343d811e6514db7
Jassocc <clnearle@gmail.com>
2023-09-14 09:28:24 -0700

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) 
+ * @head: pointer to starts
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int s = 0;
+	dlistint_t *start = head;
+
+	if (!start)
+	{
+		return (s);
+	}
+	while (start != NULL)
+	{
+		s = s + start->n;
+		start = start->next;
+	}
+	return (s);
+}
