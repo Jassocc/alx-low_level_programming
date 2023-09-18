1095d7ed1755f567fba1cb09d9909f33c651711a
Jassocc <clnearle@gmail.com>
2023-09-18 04:10:38 -0700

    task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/Jassocc/alx-low_level_programming/blob/12a46018c80c695e9f1dbb6e5540d943072875de/0x18-dynamic_libraries/libt.so
+wget -P https://github.com/Jassocc/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libt.so
 export LD_PRELOAD=/$PWD/libt.so
