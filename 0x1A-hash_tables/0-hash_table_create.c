ff92f8bb740a4f241d89833db4e141b3447449f7
Jassocc <clnearle@gmail.com>
2023-10-05 10:36:16 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	net_t->size = size;
+	new_t->size = size;
 	new_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_t->array == NULL)
 	{
